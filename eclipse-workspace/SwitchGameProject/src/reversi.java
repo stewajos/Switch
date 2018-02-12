af31d73b3e41f8b264a264fc545a7d8506b53ccc
kingj <kingj@35.40.119.187>
2018-02-12 15:33:43 -0500

    Fixed cell and reversi so that they are compatible with JavaFX board

--- eclipse-workspace/SwitchGameProject/src/reversi.java
+++ eclipse-workspace/SwitchGameProject/src/reversi.java
@@ -1,4 +1,4 @@
-
+import java.util.ArrayList;
 import java.util.*;
 import java.util.ArrayList;
 
@@ -12,6 +12,7 @@ private GameStatus status = GameStatus.IN_PROGRESS;
 /** A list of all the non-empty tiles on the board */
 private ArrayList<Cell> nonEmpty;
 
+
 public int getRows(){
 	return rows;
 }
@@ -23,6 +24,29 @@ return cols;
 public GameStatus getStatus(){
 	return status; 
 }
+public int getScore(){
+for(int i = 0; i<=rows; i++){
+	
+}
+}
+
+
+public void resizeBoard(int height, int width) {
+	// Make sure win value a power of 2, row/col valid values.
+	// -want at least a 2x2 board
+	if( height > 1 && width > 1 ) {
+		rows = height;
+		cols = width;
+		
+		
+		// create a blank 2D array (all zeros) for board
+		board = new Cell[rows][cols];
+		status = GameStatus.IN_PROGRESS;
+	}
+	else
+		throw new IllegalArgumentException();
+}
+
 public ArrayList<Cell> getNonEmptyTiles() {
 	// create a new ArrayList to store nonempty cells
 	nonEmpty = new ArrayList<Cell>();
@@ -42,5 +66,160 @@ public ArrayList<Cell> getNonEmptyTiles() {
 		}
 	}
 	return nonEmpty;
+}
+private boolean movePossible() {
+	// check if space available
+	getNonEmptyTiles(); // find all non-empty tiles
+	if( nonEmpty.size() == (rows * cols) ) {
+		// check if 2 adjacent cells have the same value if full
+		for(int i = 0; i < rows; i++)
+			for(int j = 0; j < cols; j++) {
+				if(i < rows - 1 && j < cols - 1) {
+					if(board[i][j] == board[i][j + 1] ||
+						board[i][j] == board[i + 1][j]) {
+						//status = GameStatus.IN_PROGRESS;
+						return true;
+					}
+				}
+				else if(i == rows - 1 && j < cols - 1) {
+					if(board[i][j] == board[i][j + 1]) {
+						//status = GameStatus.IN_PROGRESS;
+						return true;
+					}
+				}
+			}
+		status = GameStatus.USER_LOST;
+		return false;
+	}
+	status = GameStatus.IN_PROGRESS; // put here?
+	return true;
+}
+public boolean checkTiles(Cell c){
+	//
+	//for(int i = c.row + 1; i < getRows(); i++){
+	//check.add([i][c.getColumn])
+	//if( c.getColor() != )
+	//
+	//
+	//
+	
+	ArrayList<Cell> check = new ArrayList<Cell>();
+	//Checking to the right of the placed tile
+	Cell temp = new Cell();
+		for(int i = c.row + 1; i < getRows(); i++){
+			temp.setRow(i);
+			temp.setColumn(c.getColumn());
+			check.add(temp);
+			//adding all cells that are different color to ArrayList
+			//c.getValue might need to be changed to 0
+			//if(c.getValue() == board[i][c.column].getValue()){
+			if(board[i][c.column].getValue() == 0){
+			for(Cell d: check){
+				d.setValue(c.getValue());
+				d.setColor(c);
+			}
+			check.clear();
+				//Flip all the tiles in between
+				
+			}	
+		}
+
+		//Checking to the left of the placed tile
+		for(int i = c.row - 1; i >= 0; i--){
+		//	temp.setRow(i);
+			//temp.setColumn(c.getColumn());
+			//check.add(temp);
+			
+			if(0 == board[i][c.column].getValue()){
+				for(Cell d: check){
+					d.setValue(c.getValue());
+					d.setColor(c);
+				}
+				check.clear();
+				//Flip all the tiles in between
+			}	
+		}
+
+		//Checking above the placed tile
+		for(int j = c.column + 1; j < getCols(); j++){
+			temp.setColumn(j);
+			temp.setRow(c.getRow());
+			check.add(temp);
+			//adds tiles between selected cells into ArrayList to be changed.
+			if(0 == board[c.row][j].getValue()){
+				for(Cell d: check){
+					d.setValue(c.getValue());
+					d.setColor(c);
+				}
+				check.clear();
+				//Flip all the tiles in between
+			}	
+		}
+
+		//Checking below the placed tile
+		for(int j = c.column - 1; j >= 0; j--){
+			temp.setColumn(j);
+			temp.setRow(c.getRow());
+			check.add(temp);
+			//adds tiles between selected cells into ArrayList to be changed.
+			if(0 == board[c.row][j].getValue()){
+				for(Cell d: check){
+					d.setValue(c.getValue());
+					d.setColor(c);
+				}
+				check.clear();
+				//Flip all the tiles in between and clears the list
+			}	
+		}
+
+		//Checking the diagonal down-left of the placed tile
+		for(int i = c.row - 1; i >= 0; i--){
+			int j = c.column - c.row + i;
+			temp.setColumn(j);
+			temp.setRow(i);
+			check.add(temp);
+			if(0 == board[i][c.column].getValue()){
+				for(Cell d: check){
+					d.setValue(c.getValue());
+					d.setColor(c);
+				}
+				check.clear();
+				//Flip all the tiles in between
+			}	
+		}
+
+		//Checking the diagonal up-left of the placed tile
+		for(int i = c.row - 1; i >= 0; i--){
+			int j = c.column + c.row - i;
+			temp.setColumn(j);
+			temp.setRow(i);
+			check.add(temp);
+			if(0 == board[i][c.column].getValue()){
+				for(Cell d: check){
+					d.setValue(c.getValue());
+					d.setColor(c);
+				}
+				check.clear();
+				//Flip all the tiles in between
+				
+			}	
+		}
+
+		//Checking the diagonal down-right of the placed tile
+		for(int i = c.row + 1; i < getRows(); i++){
+			int j = c.column - c.row + i;
+			if(c.getValue() == board[i][c.column].getValue()){
+				//Flip all the tiles in between
+			}	
+		}
+
+		//Checking the diagonal up-right of the placed tile
+		for(int i = c.row + 1; i < getRows(); i++){
+			int j = c.column + c.row - i;
+			if(c.getValue() == board[i][c.column].getValue()){
+				//Flip all the tiles in between
+			}
+			return true;
+		} return true;
 }
 }
