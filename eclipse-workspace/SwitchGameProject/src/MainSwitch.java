6537e2153b80d6d5b37d86a8975339d59f9231e9
kingj <kingj@35.40.131.155>
2018-02-28 15:25:44 -0500

    Update for MainSwitch
    MainSwitch will use button and textfields to allow players to place
    pieces instead of mouse listener. Mouse LIstener will be used in final
    release.

--- eclipse-workspace/SwitchGameProject/src/MainSwitch.java
+++ eclipse-workspace/SwitchGameProject/src/MainSwitch.java
@@ -1,4 +1,6 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler; 
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.VPos;
@@ -9,14 +11,25 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage; 
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle; 
-import javafx.scene.input.MouseEvent; 
+//import javafx.scene.input.MouseEvent; 
 import javafx.scene.shape.Circle; 
+import javafx.scene.control.Button; 
+import javafx.scene.control.TextField; 
 
 public class MainSwitch extends Application{
 	final int BOARD_SIZE = 8; 
 	final int SQUARES = 64;
 	GridPane board = new GridPane(); 
 	Cell[] gamePieces = new Cell[64];
+	Rectangle[][] place = new Rectangle[8][8];
+	int piecesUsed = 4;
+	Button commitButton = new Button("Commit");
+	TextField rowField = new TextField();
+	TextField columnField = new TextField(); 
+	int row, column; 
+	reversi rules = new reversi(); 
+	rules.resizeBoard(BOARD_SIZE, BOARD_SIZE);
+	
 	
 	
 	public static void main(String args[])
@@ -26,12 +39,22 @@ public class MainSwitch extends Application{
 	}
 	public void start(Stage primaryStage) {
 		buildBoard(board);
-		putSquaresOnBoard(board);
+		board = putSquaresOnBoard(board);
 		board.setPadding(new Insets(15, 15, 15, 15));
 		Scene scene = new Scene(board, 700, 700);
 		primaryStage.setTitle("Reversi");
 		primaryStage.setScene(scene);
-		primaryStage.show(); 
+		primaryStage.show();
+		commitButton.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent e) {
+				row = Integer.parseInt(rowField.getText());
+				column = Integer.parseInt(columnField.getText());
+				gamePieces[piecesUsed] = new Cell(row, column, 1);
+				board.add(new Circle(32, gamePieces[piecesUsed].getColor()), gamePieces[piecesUsed].getRow(), gamePieces[piecesUsed].getColumn());
+				
+			}
+		});
+		
 	}
 	private void buildBoard(GridPane board) {
 		for(int i = 0; i < BOARD_SIZE; i++) {
@@ -51,11 +74,13 @@ public class MainSwitch extends Application{
 			
 		}
 	}
-	private void putSquaresOnBoard(GridPane board) {
+	private GridPane putSquaresOnBoard(GridPane board) {
 		Color[] sqColors = new Color[] {Color.GREEN, Color.DARKGREEN};
 		for(int i = 0; i < BOARD_SIZE; i++) {
 			for(int j = 0; j < BOARD_SIZE; j++) {
+				//Rectangle 
 				board.add(new Rectangle(SQUARES, SQUARES,sqColors[(i+j)%2]),i,j);
+				place[i][j] = new Rectangle(SQUARES,SQUARES,sqColors[(i+j)%2]); 
 			}
 		}
 		//add initial pieces here
@@ -63,13 +88,19 @@ public class MainSwitch extends Application{
 		gamePieces[1] = new Cell(4, 4, 2);
 		gamePieces[2] = new Cell(3, 4, 1);
 		gamePieces[3] = new Cell(3, 3, 2);
-		board.add(new Circle(32, Color.BLACK), gamePieces[0].getRow(), gamePieces[0].getColumn());
-		board.add(new Circle(32, Color.WHITE), gamePieces[1].getRow(), gamePieces[1].getColumn());
-		board.add(new Circle(32, Color.BLACK), gamePieces[2].getRow(), gamePieces[2].getColumn());
-		board.add(new Circle(32, Color.WHITE), gamePieces[3].getRow(), gamePieces[3].getColumn());
-
-	}
-	//private void placePiecesOnBoard(GridPane board) implements MouseListener{
+		board.add(new Circle(32, gamePieces[0].getColor()), gamePieces[0].getRow(), gamePieces[0].getColumn());
+		board.add(new Circle(32, gamePieces[1].getColor()), gamePieces[1].getRow(), gamePieces[1].getColumn());
+		board.add(new Circle(32, gamePieces[2].getColor()), gamePieces[2].getRow(), gamePieces[2].getColumn());
+		board.add(new Circle(32, gamePieces[3].getColor()), gamePieces[3].getRow(), gamePieces[3].getColumn());
+		board.add(rowField, 9, 9);
+		board.add(columnField, 9, 10);
+		board.add(commitButton, 9, 11);
 		
-//	}
+		
+		
+		return board;
+	}
+	private void placePiecesOnBoard(GridPane board){
+		
+	}
 }
